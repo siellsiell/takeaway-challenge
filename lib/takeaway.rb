1e0fea443a19f4bb7cee0cf78cd8e50d0d88def1
Simo Tchokni <{ID}+{username}@users.noreply.github.com>
2021-01-20 18:13:44 +0000

    Takeaway that doesn't sent SMS

---
+++ lib/takeaway.rb
@@ -0,0 +1,55 @@
+class Menu
+
+  @@dishes = [
+    { :name => :Vitamins, :price => 2 },
+    { :name => :Iron, :price => 8 },
+    { :name => :Calcium, :price => 3 },
+  ]
+
+  def self.list
+    @@dishes.map.with_index { |dish, index|
+      format("%<number>s) %<name>s %<price>.2f", number: index + 1,
+        name: dish[:name], price: dish[:price])
+    }
+  end
+
+  def self.valid_order?(dish_quantities, expected_total)
+    real_total = tally(dish_quantities, prices())
+    real_total == expected_total
+  end
+
+  private_class_method def self.prices
+    @@dishes.map { |d| d[:price] }
+  end
+
+  private_class_method def self.tally(order, prices)
+    order.map.with_index { |quantity, index|
+      prices[index] * quantity
+    }.sum
+  end
+
+end
+
+class Takeaway
+  def initialize(menu = Menu)
+    @menu = menu
+  end
+
+  def order
+    puts @menu.list()
+    print "Please enter the quantities for each dish: "
+
+    input = gets.chomp
+
+    order = input.split(" ").map(&:to_i)
+    dishes = order[0..-2]
+    total = order[-1]
+
+    unless @menu.valid_order?(dishes, total)
+      raise "Total is incorrect"
+    end
+
+    puts "Success"
+  end
+
+end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,86 @@
+require 'Takeaway'
+require 'Menu'
+
+describe "Takeaway" do
+  describe "#order" do
+    it "prints menu, allows user to order and returns success message when total is correct" do
+      menu_mock = class_double("Menu", :list => "stuff", :valid_order? => true)
+      takeaway = Takeaway.new(menu_mock)
+      allow(takeaway).to receive(:gets).and_return("1 1 1 20\n")
+      expect { takeaway.order() }.to output(
+        "stuff\n" +
+        "Please enter the quantities for each dish: " +
+        "Success\n"
+      ).to_stdout
+    end
+    it "prints menu, allows user to order and raises error when total is incorrect" do
+      menu_mock = class_double("Menu", :list => "stuff", :valid_order? => false)
+      takeaway = Takeaway.new(menu_mock)
+      allow(takeaway).to receive(:gets).and_return("1 1 1 13\n")
+      expect { takeaway.order() }.to output(
+        "stuff\n" +
+        "Please enter the quantities for each dish: "
+      ).to_stdout
+       .and raise_error(/Total is incorrect/)
+    end
+  end
+end
+
+describe "Menu" do
+
+  # TODO write test for Menu.list
+
+  describe "#valid_order?" do
+    it "returns true when expected total matches real total: 1,2,0 18" do
+      expect(Menu.valid_order?([1, 2, 0], 18)).to be true
+    end
+    it "returns true when expected total matches real total: 0,0,1 3" do
+      expect(Menu.valid_order?([0, 0, 1], 3)).to be true
+    end
+    it "returns true when expected total matches real total: 0,1,0 8" do
+      expect(Menu.valid_order?([0, 1, 0], 8)).to be true
+    end
+    it "returns true when expected total matches real total: 1,0,0 2" do
+      expect(Menu.valid_order?([1, 0, 0], 2)).to be true
+    end
+    it "returns false when expected total doesn't match real total: 1,2,0 10" do
+      expect(Menu.valid_order?([1, 2, 0], 10)).to be false
+    end
+    it "returns false when expected total doesn't match real total: 0,0,1 29" do
+      expect(Menu.valid_order?([0, 0, 1], 29)).to be false
+    end
+    it "returns false when expected total doesn't match real total: 0,1,0 0" do
+      expect(Menu.valid_order?([0, 1, 0], 0)).to be false
+    end
+    it "returns false when expected total doesn't match real total: 1,0,0 100" do
+      expect(Menu.valid_order?([0, 1, 0], 100)).to be false
+    end
+  end
+end
+
+describe "IntegrationTest" do
+  ["1 1 1 13", "1 0 3 11", "0 1 1 11"].each do |input|
+    it "prints menu, allows user to order and returns success message when total is correct" do
+      takeaway = Takeaway.new
+      allow(takeaway).to receive(:gets).and_return("#{input}\n")
+      expect { takeaway.order() }.to output(
+        "1) Vitamins 2.00\n" + 
+        "2) Iron 8.00\n" + 
+        "3) Calcium 3.00\n" + 
+        "Please enter the quantities for each dish: " +
+        "Success\n"
+      ).to_stdout
+    end
+  end
+  it "prints menu, allows user to order and raises error when total is incorrect" do
+    takeaway = Takeaway.new
+    allow(takeaway).to receive(:gets).and_return("1 1 1 20\n")
+    expect { takeaway.order() }.to output(
+      "1) Vitamins 2.00\n" + 
+      "2) Iron 8.00\n" + 
+      "3) Calcium 3.00\n" + 
+      "Please enter the quantities for each dish: "
+    ).to_stdout
+      .and raise_error(/Total is incorrect/)
+  end
+end
